cb5357eacb12d986f12b9c05ac856cddf7fc3fe0
Hernán Calvo <hernan.calvo@chediack.com.ar>
2022-11-21 19:17:25 -0300

    Primer Commit

---
+++ js/app.js
@@ -0,0 +1,140 @@
+const container = document.querySelector(".container")
+const resultado = document.querySelector("#resultado")
+const formulario = document.querySelector("#formulario")
+
+window.addEventListener("load", () => {
+  formulario.addEventListener("submit", buscarClima);
+})
+
+
+
+function buscarClima(e) {
+  e.preventDefault();
+
+  // Validar
+  const ciudad = document.querySelector("#ciudad").value;
+  const pais = document.querySelector("#pais").value;
+
+  // Consular la API
+  if(ciudad === "" || pais === "") {
+    // Hubo un error
+    mostrarError("Ambos campos son oblitaorios");
+    return;
+  }
+
+  // Consultar la API
+  consultarAPI(ciudad, pais);
+}
+
+function mostrarError(mensaje) {
+  
+  const alerta = document.querySelector(".bg-red-100");
+
+  if(!alerta) {
+     // Crear un alerta
+    const alerta = document.createElement("DIV");
+    alerta.classList.add("bg-red-100", "border-red-400", "text-red-700", "px-4", "py-3", "rounded", "max-w-md", "mx-auto", "mt-6", "text-center");
+
+    alerta.innerHTML = `
+      <strong class="font-bold">Error!</strong>
+      <span class="block">${mensaje}</span>
+    `;
+
+    container.appendChild(alerta);
+
+    setTimeout(() => {
+      alerta.remove();
+    },2000)
+  } 
+}
+
+function consultarAPI(ciudad, pais) {
+
+  
+  const appId = "d773d2a5b37d553ef6806ffc21cf6987";
+  
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+  
+  // Muestra un spinner de carga
+  spinner();
+
+  fetch(url)
+  .then( respuesta => respuesta.json())
+  .then( datos => { 
+
+      limpiarHTML();
+      if(datos.cod === "404") {
+        mostrarError("Ciudad no encontrada");
+        return;
+      }
+
+      // Imprimir la respuesta en el HTML
+      mostrarClima(datos);
+  })
+}
+
+function mostrarClima(datos) {
+  const {name, main: { temp, temp_max, temp_min } } = datos;
+
+  const centigrados = KelvinCentigrados(temp);
+  const max = KelvinCentigrados(temp_max);
+  const min = KelvinCentigrados(temp_min);
+
+  const nombreCiudad = document.createElement("P");
+  nombreCiudad.textContent = `Clima en ${name}`;
+  nombreCiudad.classList.add("font-bold", "text-2xl");
+
+  const actual = document.createElement("P");
+  actual.innerHTML = `${centigrados} &#8451;`;
+  actual.classList.add("font-bold", "text-6xl");
+
+  const tempMaxima = document.createElement("P");
+  tempMaxima.innerHTML = `Máx: ${max} &#8451;`;
+  tempMaxima.classList.add("text-xl");
+
+  const tempMinima = document.createElement("P");
+  tempMinima.innerHTML = `Min: ${min} &#8451;`;
+  tempMinima.classList.add("text-xl");
+
+  const resultadoDiv = document.createElement("DIV");
+  resultadoDiv.classList.add("text-center", "text-white");
+  resultadoDiv.appendChild(nombreCiudad);
+  resultadoDiv.appendChild(actual);
+  resultadoDiv.appendChild(tempMaxima);
+  resultadoDiv.appendChild(tempMinima);
+  
+  resultado.appendChild(resultadoDiv);
+}
+
+const KelvinCentigrados = grados => parseInt(grados - 273.15);
+
+function limpiarHTML() {
+  while(resultado.firstChild) {
+    resultado.removeChild(resultado.firstChild)
+  }
+}
+
+function spinner() {
+
+  limpiarHTML();
+
+  const divSpinner = document.createElement("DIV");
+  divSpinner.classList.add("sk-fading-circle");
+
+  divSpinner.innerHTML = `
+    <div class="sk-circle1 sk-circle"></div>
+    <div class="sk-circle2 sk-circle"></div>
+    <div class="sk-circle3 sk-circle"></div>
+    <div class="sk-circle4 sk-circle"></div>
+    <div class="sk-circle5 sk-circle"></div>
+    <div class="sk-circle6 sk-circle"></div>
+    <div class="sk-circle7 sk-circle"></div>
+    <div class="sk-circle8 sk-circle"></div>
+    <div class="sk-circle9 sk-circle"></div>
+    <div class="sk-circle10 sk-circle"></div>
+    <div class="sk-circle11 sk-circle"></div>
+    <div class="sk-circle12 sk-circle"></div>
+  `;
+
+  resultado.appendChild(divSpinner);
+}
